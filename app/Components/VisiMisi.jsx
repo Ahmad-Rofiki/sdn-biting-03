016965a92b6c7ac66f2b8437a831f25c907daf54
ahmad rofiki <ahmadrofiki6146@gmail.com>
2025-02-11 22:36:04 +0700

    awda

--- app/Components/VisiMisi.jsx
+++ app/Components/VisiMisi.jsx
@@ -5,19 +5,15 @@ export default function VisiMisi() {
     <section className="flex flex-col justify-center items-center mx-auto mt-20 h-screen text-center space-y-10 max-w-xl ">
       <div className="space-y-10 shadow-2xl rounded-lg p-10">
         <h1 className="text-5xl font-semibold">Visi</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          sequi tempore cumque, ad voluptates doloribus? Provident aliquid ab
-          quibusdam deleniti.
-        </p>
+        <p>Foya</p>
       </div>
       <div className="space-y-10 shadow-2xl rounded-lg p-10">
         <h1 className="text-5xl font-semibold">Misi</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          sequi tempore cumque, ad voluptates doloribus? Provident aliquid ab
-          quibusdam deleniti.
-        </p>
+        <p>Foya</p>
+      </div>
+      <div className="space-y-10 shadow-2xl rounded-lg p-10">
+        <h1 className="text-5xl font-semibold">VISI MISI</h1>
+        <p>FOYA FOYA</p>
       </div>
     </section>
   );
